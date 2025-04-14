a9a698c3a872f836c98c4ec76e68ef67dad8c8ca
what <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-04-14 10:41:33 -0400

    removed semicolons and error handling for reading from connectiom

--- internal/network/conn.go
+++ internal/network/conn.go
@@ -21,26 +21,32 @@ func makeMinecraftConn(logger zerolog.Logger, conn net.Conn) *MinecraftConn {
 
 func (c *MinecraftConn) Read() {
 	buffer := makeBuffer(make([]byte, 0, BufferLength))
-	c.conn.Read(buffer.data)
+	_, err := c.conn.Read(buffer.data)
+	if err != nil {
+		c.logger.Warn().Err(err).Msg("Error reading from connection")
+	}
+
 
 	packetLen, err := buffer.ReadVarInt()
 	if err != nil {
-		c.logger.Warn().Err(err).Msg("Failed to read packet length")
+		c.logger.Warn().Err(err).Msg("Error reading packet length")
 		return;
 	}
 
 	if packetLen > BufferLength {
-		targetLength := packetLen - BufferLength;
-		newData := make([]byte, 0, targetLength);
+		targetLength := packetLen - BufferLength
+		newData := make([]byte, 0, targetLength)
 
 		for len(newData) != int(targetLength) {
-			c.conn.Read(newData);
+			c.conn.Read(newData)
 		}
 
-		buffer.data = append(buffer.data, newData...);
+		buffer.data = append(buffer.data, newData...)
 	}
+
+
 
 	// TODO: add handling for multiple packets in one read
 
-	
+
 }
